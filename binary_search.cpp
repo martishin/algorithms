e975f37ca779da7f4f3e7e8a512717ec32ac0a89
Aleksei Martishin <alekseim@amazon.com>
2020-12-08 00:12:59 +0100

    improve binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 
-int binarySearchRecursive(const vector<int> &v, int l, int r, int k) {
-    if (l >= r)
+int binarySearchRecursiveIdx(const vector<int> &v, int l, int r, int k) {
+    if (l >= r) {
         return -1;
+    }
 
     int m = l + (r - l) / 2;
 
-    if (v[m] == k)
+    if (v[m] == k) {
         return m;
-    else if (v[m] > k)
-        return binarySearchRecursive(v, l, m, k);
-    else
-        return binarySearchRecursive(v, m + 1, r, k);
+    } else if (v[m] > k) {
+        return binarySearchRecursiveIdx(v, l, m, k);
+    } else {
+        return binarySearchRecursiveIdx(v, m + 1, r, k);
+    }
 }
 
 
-int binarySearchIterative(const vector<int> &v, int k) {
+int binarySearchIterativeIdx(const vector<int> &v, int k) {
     int l = 0;
     int r = v.size();
     int m;
@@ -27,15 +30,36 @@ int binarySearchIterative(const vector<int> &v, int k) {
     while (l < r) {
         m = (l + r) / 2;
 
-        if (v[m] == k)
+        if (v[m] == k) {
             return m;
-        else if (v[m] > k)
+        } else if (v[m] > k) {
             r = m;
-        else
+        } else {
             l = m + 1;
+        }
     }
 
     return -1;
+}
+
+bool binarySearch(const vector<int> &v, int k) {
+    int l = 0;
+    int r = v.size();
+    int m;
+
+    while (l < r) {
+        m = (l + r) / 2;
+
+        if (v[m] == k) {
+            return true;
+        } else if (v[m] > k) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return false;
 }
 
 size_t lowerBound(vector<int>& nums, int value) {
@@ -45,12 +69,27 @@ size_t lowerBound(vector<int>& nums, int value) {
     while (l < r) {
         auto m = l + (r - l) / 2;
 
-        if (nums[m] < value) {
-            l = m + 1;
-        } else if (nums[m] > value) {
+        if (value <= nums[m]) {
             r = m;
         } else {
-            return m;
+            l = m + 1;
+        }
+    }
+
+    return l;
+}
+
+size_t upperBound(vector<int>& nums, int value) {
+    size_t l = 0;
+    size_t r = nums.size();
+
+    while (l < r) {
+        auto m = l + (r - l) / 2;
+
+        if (value >= nums[m]) {
+            l = m + 1;
+        } else {
+            r = m;
         }
     }
 
@@ -58,14 +97,24 @@ size_t lowerBound(vector<int>& nums, int value) {
 }
 
 int main() {
-    int n, k;
+    vector<int> v {4, 6, 10, 12, 18, 20};
+    int k = 6;
 
-    cin >> n >> k;
-    vector<int> v(n);
+    for (const auto i : v) {
+        cout << i << ' ';
+    }
+    cout << endl;
+    cout << k << endl;
 
-    for (size_t i = 0; i < n; ++i)
-        cin >> v[i];
+    cout << "binarySearchRecursiveIdx: " << binarySearchRecursiveIdx(v, 0, v.size(), k) << endl;
+    cout << "binarySearchIterativeIdx: " << binarySearchIterativeIdx(v, k) << endl;
 
-    cout << binarySearchRecursive(v, 0, v.size(), k) << endl;
-    cout << binarySearchIterative(v, k) << endl;
+    cout << "binarySearch: " << binarySearch(v, k) << endl;
+    cout << "binary_search: " << binary_search(v.begin(), v.end(), k) << endl;
+
+    cout << "lowerBound: " << lowerBound(v, k) << endl;
+    cout << "lower_bound: " << lower_bound(v.begin(), v.end(), k) - v.begin() << endl;
+
+    cout << "upperBound: " << upperBound(v, k) << endl;
+    cout << "upper_bound: " << upper_bound(v.begin(), v.end(), k) - v.begin() << endl;
 }
