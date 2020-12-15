9de941887f891db7c2bb0da338d90b21d07de202
Aleksei Martishin <alekseim@amazon.com>
2020-12-15 02:47:14 +0100

    merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -5,59 +5,56 @@
 using namespace std;
 
 
-void mergeSortMerge(vector<int> &v, int l, int m, int r);
+void merge(vector<int>& nums, int l, int m, int r) {
+    vector<int> leftSubarray;
+    vector<int> rightSubarray;
 
+    for (size_t i = l; i <= r; ++i) {
+        if (i <= m) {
+            leftSubarray.push_back(nums[i]);
+        } else {
+            rightSubarray.push_back(nums[i]);
+        }
+    }
+
+    int leftPointer = 0;
+    int rightPointer = 0;
+
+    while (leftPointer < leftSubarray.size() && rightPointer < rightSubarray.size()) {
+        if (leftSubarray[leftPointer] <= rightSubarray[rightPointer]) {
+            nums[l++] = leftSubarray[leftPointer++];
+        } else {
+            nums[l++] = rightSubarray[rightPointer++];
+        }
+    }
+
+    for (size_t i = leftPointer; i < leftSubarray.size(); ++i) {
+        nums[l++] = leftSubarray[i];
+    }
+
+    for (size_t i = rightPointer; i < rightSubarray.size(); ++i) {
+        nums[l++] = rightSubarray[i];
+    }
+}
 
 void mergeSortRecursive(vector<int> &v, int l, int r) {
     if (l < r) {
         int m = (l + r) / 2;
         mergeSortRecursive(v, l, m);
         mergeSortRecursive(v, m + 1, r);
-        mergeSortMerge(v, l, m, r);
+        merge(v, l, m, r);
     }
 }
 
-
 void mergeSortIterative(vector<int> &v, int n) {
-    for (size_t c = 1; c <= n - 1; c *= 2) {
+    for (size_t c = 1; c < n; c *= 2) {
         for (size_t l = 0; l < n - 1; l += 2 * c) {
             int m = l + c - 1;
             int r = min((int)(l + 2 * c - 1), n - 1);
-            mergeSortMerge(v, l, m, r);
+            merge(v, l, m, r);
         }
     }
 }
-
-
-void mergeSortMerge(vector<int> &v, int l, int m, int r) {
-    int lp = 0, rp = 0;
-    vector<int> L, R;
-
-    for (size_t i = l; i <= r; ++i) {
-        if (i <= m) {
-            L.push_back(v[i]);
-        } else {
-            R.push_back(v[i]);
-        }
-    }
-
-    while (lp < L.size() && rp < R.size()) {
-        if (L[lp] <= R[rp]) {
-            v[l++] = L[lp++];
-        } else {
-            v[l++] = R[rp++];
-        }
-    }
-
-    while (lp < L.size()) {
-        v[l++] = L[lp++];
-    }
-
-    while (rp < R.size()) {
-        v[l++] = R[rp++];
-    }
-}
-
 
 int main() {
     int n;
